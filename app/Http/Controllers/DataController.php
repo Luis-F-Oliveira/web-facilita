39fa3750e6250a7824d83968f4af5d5e01cbfcea
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2025-02-04 17:45:11 -0300

    consertando bug do paginate e adicionado busca exata

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Data;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DataController extends Controller
@@ -18,29 +19,31 @@ class DataController extends Controller
     {
         $query = Data::query()->with('servant');
 
-        if ($request->filled('portaria')) {
-            $query->where('order', $request->query('portaria'));
+        $filters = $request->query();
+
+        if (!empty($filters['portaria'])) {
+            $query->where('order', 'LIKE', '%' . $filters['portaria'] . '%');
         }
 
-        if ($request->filled('nome')) {
-            $query->whereHas('servant', function ($q) use ($request) {
-                $q->where('name', 'like', '%' . $request->query('nome') . '%');
+        if (!empty($filters['nome'])) {
+            $query->whereHas('servant', function ($q) use ($filters) {
+                $q->where('name', 'like', '%' . $filters['nome'] . '%');
             });
         }
 
-        if ($request->filled('email')) {
-            $query->whereHas('servant', function ($q) use ($request) {
-                $q->where('email', 'like', '%' . $request->query('email') . '%');
+        if (!empty($filters['email'])) {
+            $query->whereHas('servant', function ($q) use ($filters) {
+                $q->where('email', 'like', '%' . $filters['email'] . '%');
             });
         }
 
-        if ($request->filled('data')) {
-            $date = \Carbon\Carbon::createFromFormat('Y-m-d', $request->query('data'))->format('Y-m-d');
+        if (!empty($filters['data'])) {
+            $date = Carbon::createFromFormat('Y-m-d', $filters['data'])->format('Y-m-d');
             $query->whereDate('created_at', $date);
         }
 
-        $data = $query->paginate(15);
+        $data = $query->paginate(15)->appends($request->query());
 
-        return view("principal.index", compact('data'));
+        return view("principal.index", compact('data', 'filters'));
     }
 }
--- resources/views/principal/partials/filter.blade.php
+++ resources/views/principal/partials/filter.blade.php
@@ -1,12 +1,12 @@
 <form class="mb-4 bg-gray-200 rounded-lg" method="GET" action="{{ route('dashboard.show') }}">
   <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-      <x-text-input type="text" name="portaria" placeholder="Portaria" class="w-full" />
-      <x-text-input type="text" name="nome" placeholder="Nome" class="w-full" />
-      <x-text-input type="email" name="email" placeholder="E-mail" class="w-full" />
-      <x-text-input type="date" name="data" class="w-full" />
+      <x-text-input type="text" name="portaria" placeholder="Portaria" class="w-full" value="{{ request('portaria') }}" />
+      <x-text-input type="text" name="nome" placeholder="Nome" class="w-full" value="{{ request('nome') }}" />
+      <x-text-input type="email" name="email" placeholder="E-mail" class="w-full" value="{{ request('email') }}" />
+      <x-text-input type="date" name="data" class="w-full" value="{{ request('data') }}" />
   </div>
   <div class="w-full flex justify-end mt-4">
-      <a href="/dashboard">
+      <a href="{{ route('dashboard.show') }}">
         <x-secondary-button type="button">Limpar</x-button>
       </a>
       <x-primary-button class="ml-3" type="submit">Filtrar</x-button>
