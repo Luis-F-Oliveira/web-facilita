8ed1b08706588b6920c09eba21a2b3bae799267c
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2025-02-05 16:28:32 -0300

    atualizando layout das tabelas

--- resources/views/principal/partials/table.blade.php
+++ resources/views/principal/partials/table.blade.php
@@ -1,21 +1,21 @@
-<table class="w-full border-collapse border border-gray-500 dark:text-white">
+<table class="w-full divide-y divide-gray-200 dark:divide-neutral-700">
   <thead>
     <tr>
-      <th class="border border-gray-500 px-4 py-2 text-left">Portaria</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Nome</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">E-mail</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Data</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">URL</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Portaria</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Nome</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">E-Mail</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Data</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">URL</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
     @foreach ($data as $item)
     <tr>
-      <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->order }}</td>
-      <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->servant->name }}</td>
-      <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->servant->email }}</td>
-      <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->created_at }}</td>
-      <td class="border border-gray-500 px-4 py-2 text-sm">
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->order }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->servant->name }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->servant->email }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->created_at }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
         <a href="{{ $item->url }}" target="_blank" rel="noopener noreferrer">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-arrow-out-up-right">
             <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
@@ -28,6 +28,7 @@
     @endforeach
   </tbody>
 </table>
+
 <div class="mt-4">
   {!! $data->links() !!}
 </div>
--- resources/views/servants/partials/table.blade.php
+++ resources/views/servants/partials/table.blade.php
@@ -1,43 +1,40 @@
-<table class="w-full border-collapse border border-gray-500 dark:text-white">
+<table class="w-full divide-y divide-gray-200 dark:divide-neutral-700">
   <thead>
     <tr>
-      <th class="border border-gray-500 px-4 py-2 text-left">Matrícula</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Contrato</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Nome</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">E-mail</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Notificações</th>
-      <th class="border border-gray-500 px-4 py-2 text-left">Editar</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Matrícula</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Contrato</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Nome</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">E-mail</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Notificações</th>
+      <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Editar</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
     @foreach ($data as $item)
-      <tr>
-        <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->enrollment }}</td>
-        <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->contract }}</td>
-        <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->name }}</td>
-        <td class="border border-gray-500 px-4 py-2 text-sm">{{ $item->email }}</td>
-        <td class="border border-gray-500 px-4 py-2 text-sm">
-          @if ($item->active)
-            <span>Ativo</span>
-          @else
-            <span>Inativo</span>
-          @endif
-        </td>
-        <td class="border border-gray-500 px-4 py-2 text-sm">
-          <a href="{{ route('servants.edit', $item->id) }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-              stroke-linejoin="round" class="lucide lucide-pencil">
-              <path
-                d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-              <path d="m15 5 4 4" />
-            </svg>
-          </a>
-        </td>
-      </tr>
+    <tr>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->enrollment }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->contract }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->name }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">{{ $item->email }}</td>
+      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+        @if ($item->active)
+          <span>Ativo</span>
+        @else
+          <span>Inativo</span>
+        @endif
+      </td>
+      <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+        <a href="{{ route('servants.edit', $item->id) }}">
+          <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent focus:outline-none disabled:opacity-50 disabled:pointer-events-none">
+            Editar
+          </button>
+        </a>
+      </td>
+    </tr>
     @endforeach
   </tbody>
 </table>
+
 <div class="mt-4">
   {!! $data->links() !!}
 </div>
